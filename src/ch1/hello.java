2bc18e165b27b8424f3b7394b174b3618728bd8d
dciv <kkcf0123@gmail.com>
2024-01-30 17:49:59 +0900

    20240130 java main

---
+++ src/ch1/hello.java
@@ -0,0 +1,16 @@
+package ch1;
+
+public class hello {
+    public static void main(String[] args) {
+        int var1 = 5;
+        System.out.println("guess what? : " + var1);
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+    }
+
+    public static void sub(String[] args) {
+
+    }
+}
